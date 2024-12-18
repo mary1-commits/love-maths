df0aae2aec59dd49de9b20211ad46f1a1267fed2
mary1-commits aden <maryansaeed67@gmail.com>
2024-12-18 11:42:38 +0000

    display the addition question

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -13,13 +13,21 @@ for (let button of buttons) {
 }
        let gameType = this.getAttribute("data-type");
        alert('You clicked ${gameType}');
-
+ 
+       runGame("addition");
 
 function runGame() {
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+} 
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+} else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
 }
+
 function checkAnswer() {
 
 }
@@ -33,6 +41,10 @@ function incrementWrongAnswer() {
 
 }
 function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 
 }
 function displaySubtractQuestion() {
